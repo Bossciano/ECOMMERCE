6e220b4772563269922283be55176283d8a4ac9c
B0SS-C1AN0 <louiscolix@gmail.com>
2026-01-22 05:58:30 +0100

    Enable WishlistProvider in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { CartProvider } from "@/contexts/CartContext";
-//import { WishlistProvider } from "@/contexts/WishlistProvider";
+import { WishlistProvider } from "@/contexts/WishlistProvider";
 import Index from "./pages/Index";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
@@ -40,6 +40,7 @@ const App = () => (
               </Routes>
             </BrowserRouter>
           </CartProvider>
+  
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
